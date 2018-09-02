1a6afeb1a0ef5ab7087dbea3b0363fdea2644768
开源oschina <115889198@qq.com>
2018-09-03 00:52:05 +0800

    MD5不安全 阿里规范报警

--- initial_version_of_springboot/src/main/java/com/fc/test/util/MD5Util.java
+++ initial_version_of_springboot/src/main/java/com/fc/test/util/MD5Util.java
@@ -27,6 +27,7 @@ public class MD5Util {
 		for (int i = 0; i < charArray.length; i++){
 			byteArray[i] = (byte) charArray[i];
 		}
+			
 		byte[] md5Bytes = md5.digest(byteArray);
 		StringBuffer hexValue = new StringBuffer();
 		for (int i = 0; i < md5Bytes.length; i++) {
@@ -34,8 +35,7 @@ public class MD5Util {
 			if (val < 16) {
 				hexValue.append("0");
 			}
- 
-			hexValue.append(Integer.toHexString(val));
+			hexValue.append(String.format("%02X",val));
 		}
 		return hexValue.toString();
 	}
