9c6353ddf2284bef4acd930790204c9a36007b91
开源oschina <115889198@qq.com>
2018-09-12 14:55:59 +0800

    添加Google验证码

--- initial_version_of_springboot/src/main/java/com/fc/test/shiro/config/ShiroFilterMapFactory.java
+++ initial_version_of_springboot/src/main/java/com/fc/test/shiro/config/ShiroFilterMapFactory.java
@@ -42,6 +42,8 @@ user:例如/admins/user/**=user没有参数表示必须存在用户，当登入�
 		filterChainDefinitionMap.put("/login", "anon");
 		filterChainDefinitionMap.put("/login", "anon");
 		filterChainDefinitionMap.put("/logout", "logout");
+		//放验证码
+		filterChainDefinitionMap.put("/captcha/captchaImage**", "anon");
 		//对所有页面进行认证
 		filterChainDefinitionMap.put("/**","authc");
 		return filterChainDefinitionMap;
