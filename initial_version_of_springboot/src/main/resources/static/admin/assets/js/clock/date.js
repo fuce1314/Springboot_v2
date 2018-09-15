a767662f8126fc5b377bd181067c6f941d440e06
开源oschina <115889198@qq.com>
2018-09-15 23:20:26 +0800

    修改时间

--- initial_version_of_springboot/src/main/resources/static/admin/assets/js/clock/date.js
+++ initial_version_of_springboot/src/main/resources/static/admin/assets/js/clock/date.js
@@ -1,8 +1,6 @@
-var monthNames = ["January", "February", "March", "April", "May", "June",
-    "July", "August", "September", "October", "November", "December"
-];
-var dayNames = ["Sun, ", "Mon, ", "Tue, ", "Wed, ", "Thu, ", "Fri, ", "Sat, "]
+var monthNames = ["一月","二月","三月","四月","五月","六月","七月","八月","九月","十月","十一月","十二月"];
+var dayNames = ["星期天, ","星期一, ", "星期二, ", "星期三, ", "星期四, ", "星期五, ", "星期六, "]
 
 var newDate = new Date();
-newDate.setDate(newDate.getDate() + 1);
+newDate.setDate(newDate.getDate());
 $('#Date').html(dayNames[newDate.getDay()] + " " + newDate.getDate() + ' ' + monthNames[newDate.getMonth()] + ' ' + newDate.getFullYear());
