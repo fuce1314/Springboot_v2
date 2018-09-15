f76723d3738db762feb40f855a0c431d4b015246
开源oschina <115889198@qq.com>
2018-09-15 23:29:18 +0800

    whcrow 同学提出的修改test测试类放入test包里面，修改md里面的mybatis1.3.2为mybatis-spring 1.3.2

--- initial_version_of_springboot/src/main/java/com/fc/test/controller/admin/CaptchaController.java
+++ initial_version_of_springboot/src/main/java/com/fc/test/controller/admin/CaptchaController.java
@@ -2,20 +2,16 @@ package com.fc.test.controller.admin;
 
 import java.awt.image.BufferedImage;
 import java.io.IOException;
-
-import javax.annotation.Resource;
 import javax.imageio.ImageIO;
 import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
-
 import com.fc.test.common.base.BaseController;
 import com.google.code.kaptcha.Constants;
 import com.google.code.kaptcha.Producer;
