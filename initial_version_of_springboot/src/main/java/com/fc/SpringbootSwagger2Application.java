c9e39f8281aaa2d5566b60dca6f0fac38f678a65
Administrator <Administrator@CNKOT7BCBCID54B>
2019-02-21 22:26:38 +0800

    添加注释

--- initial_version_of_springboot/src/main/java/com/fc/SpringbootSwagger2Application.java
+++ initial_version_of_springboot/src/main/java/com/fc/SpringbootSwagger2Application.java
@@ -4,6 +4,12 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+/**
+ * 项目启动方法
+ * @author Administrator
+ *
+ */
 @SpringBootApplication
 @MapperScan(value = "com.fc.test.mapper")
 public class SpringbootSwagger2Application {
