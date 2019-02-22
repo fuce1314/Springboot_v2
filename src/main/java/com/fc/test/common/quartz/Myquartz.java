e220bf8c87227c617239c52d05ce9918f61523c0
fc <115889198@qq.com>
2019-02-22 14:51:56 +0800

    注释控制台输出代码

--- src/main/java/com/fc/test/common/quartz/Myquartz.java
+++ src/main/java/com/fc/test/common/quartz/Myquartz.java
@@ -26,8 +26,8 @@ public class Myquartz {
     	//test=testService.queryId(test);
     	//Gson gson=new Gson();
         //获取当前时间
-        LocalDateTime localDateTime =LocalDateTime.now();
-        System.out.println("当前时间为:" + localDateTime.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
+       // LocalDateTime localDateTime =LocalDateTime.now();
+       // System.out.println("当前时间为:" + localDateTime.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
     }
     
     
