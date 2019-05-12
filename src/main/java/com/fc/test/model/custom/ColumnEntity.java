c0cfcf1280d0e0a7a740bba31793f1ba133c2d37
Administrator <Administrator@OMBHKIY656N5G8N>
2019-05-12 19:32:37 +0800

    mapper类型映射bug修改

--- src/main/java/com/fc/test/model/custom/ColumnEntity.java
+++ src/main/java/com/fc/test/model/custom/ColumnEntity.java
@@ -23,6 +23,9 @@ public class ColumnEntity {
     private String attrType;
     //auto_increment
     private String extra;
+    //mapper映射类型
+    private String mappdataType = getMappdataType();
+    
     
 	public String getColumnName() {
 		return columnName;
@@ -66,4 +69,82 @@ public class ColumnEntity {
 	public void setExtra(String extra) {
 		this.extra = extra;
 	}
+	//数据库常用字段映射关系
+	public String getMappdataType() {
+		String type=this.getDataType();
+		String returnStr="VARCHAR";
+		if(type==null) {
+			return null;
+		}
+		switch(type) {
+			case "tinyint" :
+				returnStr="INTEGER";
+				break;
+			case "smallint" :
+				returnStr="INTEGER";
+				break;
+			case "int" :
+				returnStr="INTEGER";
+				break;
+			case "bigint" :
+				returnStr="BIGINT";
+				break;
+			case "mediumint" :
+				returnStr="INTEGER";
+				break;
+			case "integer" :
+				returnStr="INTEGER";
+				break;
+			case "float" :
+				returnStr="REAL";
+				break;
+			case "double" :
+				returnStr="DOUBLE";
+				break;
+			case "decimal" :
+				returnStr="DECIMAL";
+				break;
+			case "bit" :
+				returnStr="OTHER";
+				break;
+			case "char" :
+				returnStr="CHAR";
+				break;
+			case "varchar" :
+				returnStr="VARCHAR";
+				break;
+			case "tinytext" :
+				returnStr="VARCHAR";
+				break;
+			case "text" :
+				returnStr="VARCHAR";
+				break;
+			case "mediumtext" :
+				returnStr="VARCHAR";
+				break;
+			case "longtext" :
+				returnStr="VARCHAR";
+				break;
+			case "date" :
+				returnStr="TIMESTAMP";
+				break;
+			case "datetime" :
+				returnStr="TIMESTAMP";
+				break;
+			case "timestamp" :
+				returnStr="TIMESTAMP";
+				break;
+			default:
+				break;
+		}
+		return returnStr;
+	}
+	public void setMappdataType(String mappdataType) {
+		this.mappdataType = mappdataType;
+	}
+	
+
+	
+	
+	
 }
