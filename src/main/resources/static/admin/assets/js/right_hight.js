5fd412ae54be5fc9e4d6d611d3bcb0f235261d7e
开源oschina <115889198@qq.com>
2019-06-09 01:37:34 +0800

    右边高度自适应修复完毕

---
+++ src/main/resources/static/admin/assets/js/right_hight.js
@@ -0,0 +1,7 @@
+//控制右边高度得js
+function reinitIframe(){
+		var $parent =window.parent.document.getElementById("mainFrame");
+		$($parent).attr("height",document.body.scrollHeight);
+		
+	}
+window.setInterval("reinitIframe()", 200);
