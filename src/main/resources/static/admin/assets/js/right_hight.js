3255d90235b6984a981117fda05a0cf26eb50d33
开源oschina <115889198@qq.com>
2019-06-14 02:19:58 +0800

    修改右边方案

--- src/main/resources/static/admin/assets/js/right_hight.js
+++ src/main/resources/static/admin/assets/js/right_hight.js
@@ -1,7 +1,7 @@
 //控制右边高度得js
-function reinitIframe(){
-		var $parent =window.parent.document.getElementById("mainFrame");
-		$($parent).attr("height",document.body.scrollHeight);
-		
-	}
-window.setInterval("reinitIframe()", 200);
+//function reinitIframe(){
+//		var $parent =window.parent.document.getElementById("mainFrame");
+//		$($parent).attr("height",document.body.scrollHeight);
+//		
+//	}
+//window.setInterval("reinitIframe()", 200);
