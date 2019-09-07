381a5d9be39e82d2433ffc53d34944908b2cd01f
开源oschina <115889198@qq.com>
2019-09-08 00:58:17 +0800

    status  bug修复

--- src/main/resources/static/admin/assets/js/preloader.js
+++ src/main/resources/static/admin/assets/js/preloader.js
@@ -1,5 +1,5 @@
     $(function() { // makes sure the whole site is loaded
-        $('#status').fadeOut(); // will first fade out the loading animation
+        $('#divstatus').fadeOut(); // will first fade out the loading animation
         $('#preloader').fadeOut(); // will fade out the white DIV that covers the website.
         $('body').delay(350).css({
             'overflow': 'visible'
