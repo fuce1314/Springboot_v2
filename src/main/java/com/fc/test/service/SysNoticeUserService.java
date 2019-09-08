111759006ae72f9665384768615f117a83c65992
开源oschina <115889198@qq.com>
2019-09-08 19:53:51 +0800

    去黄色警告

--- src/main/java/com/fc/test/service/SysNoticeUserService.java
+++ src/main/java/com/fc/test/service/SysNoticeUserService.java
@@ -1,6 +1,5 @@
 package com.fc.test.service;
 
-import java.util.ArrayList;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
