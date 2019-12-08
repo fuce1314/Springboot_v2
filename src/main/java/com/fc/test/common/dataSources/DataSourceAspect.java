306757f57421557db8acfe263328923a7ab628c0
开源oschina <115889198@qq.com>
2019-12-08 11:13:23 +0800

    去警告

--- src/main/java/com/fc/test/common/dataSources/DataSourceAspect.java
+++ src/main/java/com/fc/test/common/dataSources/DataSourceAspect.java
@@ -5,8 +5,6 @@ import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
 import org.aspectj.lang.reflect.MethodSignature;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
