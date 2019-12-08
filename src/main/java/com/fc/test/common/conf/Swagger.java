9b1680c82657a7e69d708ac7bc5a550116771bcc
开源oschina <115889198@qq.com>
2019-12-08 11:11:42 +0800

    去警告

--- src/main/java/com/fc/test/common/conf/Swagger.java
+++ src/main/java/com/fc/test/common/conf/Swagger.java
@@ -7,7 +7,6 @@ import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
