9bf2fc5f449a2ba2700a9c3a264a51563b728b88
开源oschina <115889198@qq.com>
2020-04-10 23:44:49 +0800

    添加通用查询方法

--- src/main/java/com/fc/test/mapper/custom/SysUtilDao.java
+++ src/main/java/com/fc/test/mapper/custom/SysUtilDao.java
@@ -1,5 +1,8 @@
 package com.fc.test.mapper.custom;
 
+import java.util.List;
+import java.util.Map;
+
 import com.fc.test.model.custom.SQLAdapter;
 
 /**
@@ -19,4 +22,14 @@ public interface SysUtilDao {
 	 * @Date 2019年8月31日 下午6:15:08
 	 */
 	public int executeSQL(SQLAdapter sql); 
+	
+	
+	/**
+	 * 查询sql
+	 * @param sql
+	 * @return
+	 * @author fuce
+	 * @Date 2020年4月10日 下午11:43:16
+	 */
+	public List<Map<Object,Object>> SelectExecuteSQL(SQLAdapter sql); 
 }
--- src/main/java/com/fc/test/service/SysUtilService.java
+++ src/main/java/com/fc/test/service/SysUtilService.java
@@ -1,5 +1,8 @@
 package com.fc.test.service;
 
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,5 +23,17 @@ public class SysUtilService {
 	 */
 	public int executeSQL(String sql){
 		return dao.executeSQL(new SQLAdapter(sql));
+	}
+	
+	
+	/**
+	 * 查询sql
+	 * @param sql
+	 * @return list<map>
+	 * @author fuce
+	 * @Date 2020年4月10日 下午4:55:49
+	 */
+	public List<Map<Object,Object>> SelectExecuteSQL(String sql){
+		return dao.SelectExecuteSQL(new SQLAdapter(sql));
 	}
 }
