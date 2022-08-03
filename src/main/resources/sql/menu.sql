4a78be6caef518feda88518eed1e3a275861a5cd
开源oschina <115889198@qq.com>
2022-08-03 17:08:56 +0800

    解决302

--- src/main/resources/sql/menu.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO `t_sys_permission` VALUES ('660347913305722880', '磁盘柜号管理', '磁盘柜号展示', '/CpghController/view','0', '0', 'gen:cpgh:view', 1, 'layui-icon layui-icon-face-smile', NULL,0)
-,('660347913305722881', '磁盘柜号集合', '磁盘柜号集合', '/CpghController/list','0', '660347913305722880', 'gen:cpgh:list', 2, '', NULL,0)
-,('660347913305722882', '磁盘柜号添加', '磁盘柜号添加', '/CpghController/add','0', '660347913305722880', 'gen:cpgh:add', 2, 'layui-icon layui-icon-add-1', NULL,0)
-,('660347913305722883', '磁盘柜号删除', '磁盘柜号删除', '/CpghController/remove','0', '660347913305722880', 'gen:cpgh:remove', 2, 'layui-icon layui-icon-delete', NULL,0)
-,('660347913305722884', '磁盘柜号修改', '磁盘柜号修改', '/CpghController/edit','0', '660347913305722880', 'gen:cpgh:edit', 2, 'layui-icon layui-icon-edit', NULL,0);
-
